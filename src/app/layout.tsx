45509b34dd5f8d5e34ea43751802b52755a8586f
ekajaya740 <ekajaya740@gmail.com>
2024-08-20 20:18:45 +0800

    chore: modularize fonts call

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,8 @@ import TopNavigation from '@/components/navigations/TopNavigation';
 import './globals.css';
 import { Providers } from './providers';
 import { navigationItems } from '@/lib/items/navigationItem';
-import { Poppins } from 'next/font/google';
 import Footer from '@/components/footer/Footer';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
+import { poppins } from '@/lib/fonts/poppins';
 
 export default function RootLayout({
   children,
