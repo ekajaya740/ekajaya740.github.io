1cc657ce0144478f0882681070afa8544feb85dc
ekajaya740 <ekajaya740@gmail.com>
2024-09-20 21:23:08 +0800

    feat: add schema for FeaturedProject

--- src/lib/schemas/project.ts
+++ src/lib/schemas/project.ts
@@ -1,11 +1,20 @@
 import { z } from 'zod';
 
 export const BaseProjectSchema = z.object({
-  src: z.string(),
-  alt: z.string(),
+  thumbnail: z.string(),
   title: z.string(),
   period: z.string(),
+  description: z.string(),
+  content: z.string(),
   href: z.string(),
 });
 
+export const FeaturedProjectSchema = BaseProjectSchema.omit({
+  description: true,
+  content: true,
+  href: true,
+});
+
 export type BaseProjectType = z.infer<typeof BaseProjectSchema>;
+
+export type FeaturedProjectType = z.infer<typeof FeaturedProjectSchema>;
