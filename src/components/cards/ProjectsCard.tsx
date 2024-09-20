bd5d18c6462ebcf025088dec60d6e8b1114c5431
ekajaya740 <ekajaya740@gmail.com>
2024-09-20 21:22:17 +0800

    fix: change HyperUI style image to NextUI Image

--- src/components/cards/ProjectsCard.tsx
+++ src/components/cards/ProjectsCard.tsx
@@ -1,26 +1,25 @@
 import HyperUIImage from '../images/HyperUIImage';
-import { BaseProjectType } from '@/lib/schemas/project';
-import { Link } from '@nextui-org/react';
+import { FeaturedProjectType } from '@/lib/schemas/project';
+import { Link, Image } from '@nextui-org/react';
 
-export default function ProjectsCard(props: BaseProjectType) {
+export default function ProjectsCard(props: FeaturedProjectType) {
   return (
-    <Link className='flex flex-col space-y-4 w-full group' href={props.href}>
-      <HyperUIImage
-        image={{
-          src: 'https://static-cse.canva.com/blob/1300375/create_Free-Mockup-Generator_desktop2.jpg',
-          alt: '',
-          width: 200,
-          height: 200,
-        }}
-      />
-      <div className='w-full'>
-        <div className='place-content-between gap-3 grid grid-cols-3'>
-          <h3 className='col-span-2 line-clamp-2 font-bold text-2xl'>
-            {props.title}
-          </h3>
-          <p className='font-light text-sm place-self-end self-start'>
-            {props.period}
-          </p>
+    <Link className='flex flex-col space-y-4 w-full'>
+      <div className='relative'>
+        <Image
+          src={props.thumbnail}
+          alt={`Thumbnail of ${props.title}`}
+          className='-z-10 rounded-md w-full aspect-video object-cover'
+        />
+        <div className='md:right-0 md:bottom-0 md:left-0 md:absolute md:bg-black/50 rounded-md w-full'>
+          <div className='place-content-between gap-3 grid grid-cols-3 mt-4 md:p-8'>
+            <h3 className='col-span-2 line-clamp-2 font-bold text-black text-lg md:text-2xl md:text-white'>
+              {props.title}
+            </h3>
+            <p className='font-light text-black text-sm md:text-base md:text-white place-self-end self-start'>
+              {props.period}
+            </p>
+          </div>
         </div>
       </div>
     </Link>
